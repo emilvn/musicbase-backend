37728e964d50d48232a7e7310552174ba88f7dfc
emilvn <emilvnielsen@hotmail.com>
2023-09-14 11:22:06 +0200

    packagelock

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(morgan("tiny"));
+
+app.use("/tracks/", tracksRouter);
+
+app.use("/artists/", artistsRouter);
+
+app.use("/albums/", albumsRouter);
+
+
+app.listen(port,() => {
+	console.log(`Server is running at http://localhost:${port}`);
+})
