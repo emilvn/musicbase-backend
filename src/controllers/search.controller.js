010878cafa14998047334eec68ac5d6e8e0576ae
Viktor August Rattleff <varattleff@Viktors-MacBook-Air.local>
2023-09-15 11:49:52 +0200

    search value kommer op i postman

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -2,12 +2,11 @@ import { json } from "express";
 import { connection } from "../../config/database.js";
 
 export function getAllSearchReq(req, res){
-  const query = "call search('summer');"
+  const query = "call search('su');"
   connection.query(query, (error, resulst, fields) => { 
     if(error){
       console.log(error);
     } else {
-      console.log(json(resulst))
       res.json(resulst)
     }
   })
