342e7f6eb18b4707d9d6ed3bf48b4c21b5168320
Viktor August Rattleff <varattleff@Viktors-MacBook-Air.local>
2023-09-15 08:41:00 +0200

    update med req, res

--- src/controllers/tracks.controller.js
+++ src/controllers/tracks.controller.js
@@ -1,49 +1,49 @@
 import { connection } from "../../database.js";
 
 //Handle getting all the tracks
-export function getAllTracks(request, response) {
+export function getAllTracks(req, res) {
     const query = "SELECT * FROM tracks;";
     connection.query(query, (error, results, fields) => {
         if (error) {
             console.log(error);
         } else {
-            response.json(results);
+            res.json(results);
         }
     });
 }
 
 //Handle getting a specific track 
-export function getSpecificTrack(request, response) {
-    const id = request.params.id;
+export function getSpecificTrack(req, res) {
+    const id = req.params.id;
     const query = "SELECT * FROM tracks WHERE id = ?;";
     const values = [id];
     connection.query(query, values, (error, results) => {
         if (error) {
             console.log(error);
         } else {
-            response.json(results[0]);
+            res.json(results[0]);
         }
     });
 }
 
 //handle adding a track to the database
-export function addTrack(request, response) {
-    const tracks = request.body;
+export function addTrack(req, res) {
+    const tracks = req.body;
     const query = "INSERT INTO tracks(track_name) VALUES(?);";
     const values = [tracks.track_name];
     connection.query(query, values, (error, results) => {
         if (error) {
             console.log(error);
         } else {
-            response.json(results);
+            res.json(results);
         }
     });
 }
 
 //handle updating a specifik track using ID
-export function updateTracksByID(request, response) {
-    const id = request.params.id;
-    const track = request.body;
+export function updateTracksByID(req, res) {
+    const id = req.params.id;
+    const track = req.body;
     const query = "UPDATE tracks SET track_name = ? WHERE id = ?;";
     const values = [track.track_name, id];
 
@@ -51,14 +51,14 @@ export function updateTracksByID(request, response) {
         if (error) {
             console.log(error);
         } else {
-            response.json(results);
+            res.json(results);
         }
     });
 }
 
 //handle deleting a specific track using ID
-export function deleteTrackByID(request, response) {
-    const id = request.params.id;
+export function deleteTrackByID(req, res) {
+    const id = req.params.id;
     const query = "DELETE FROM tracks WHERE id =?;";
     const values = [id];
 
@@ -66,7 +66,7 @@ export function deleteTrackByID(request, response) {
         if (error) {
             console.log(error);
         } else {
-            response.json(results);
+            res.json(results);
         }
     });
 }
