66b8b905f71e6d4808a5acabcce48f68d1b15403
emilvn <emilvnielsen@hotmail.com>
2023-09-15 08:32:37 +0200

    changed to use dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import {errorHandler} from "./src/middleware/errorhandler.js";
+import {connection} from "./config/database.js";
 
 const app = express();
 const port = 3000;
@@ -17,8 +18,6 @@ app.use(morgan("tiny"));
 //app.use("/artists/", artistsRouter);
 
 //app.use("/albums/", albumsRouter);
-
-
 app.use(errorHandler);
 app.listen(port,() => {
 	console.log(`Server is running at http://localhost:${port}`);
