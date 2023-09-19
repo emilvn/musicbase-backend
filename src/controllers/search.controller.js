641583b8b6c09c52ea504b75d348812d944248f5
emilvn <emilvnielsen@hotmail.com>
2023-09-19 23:05:48 +0200

    docs for controllers

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,7 +1,16 @@
 import { connection } from "../../config/database.js";
 import {validateSearchQuery} from "../validation/search.validation.js";
+// noinspection ES6UnusedImports
+import express from "express";
 
+/**
+ * finds artists on the database matching the search query and sends results to the client
+ * @param {express.Request} req incoming request object
+ * @param {express.Response} res outgoing response, for sending response to client
+ * @param {express.NextFunction} next callback function to pass control to next middleware
+ */
 export function searchArtists(req, res, next){
+	// searchArtists procedure, see /database_docs/procedures/searchArtists.md
 	const query = "CALL searchArtists(?);"
 	const value = req.query.q
 	try{
@@ -14,7 +23,15 @@ export function searchArtists(req, res, next){
 		next(err);
 	}
 }
+
+/**
+ * finds albums on the database matching the search query and sends the results to the client
+ * @param {express.Request} req incoming request object
+ * @param {express.Response} res outgoing response, for sending response to client
+ * @param {express.NextFunction} next callback function to pass control to next middleware
+ */
 export function searchAlbums(req, res, next){
+	// searchAlbums procedure, see /database_docs/procedures/searchAlbums.md
 	const query = "CALL searchAlbums(?);"
 	const value = req.query.q
 	try{
@@ -27,7 +44,15 @@ export function searchAlbums(req, res, next){
 		next(err);
 	}
 }
+
+/**
+ * finds tracks on the database matching the search query and sends the results to the client
+ * @param {express.Request} req incoming request object
+ * @param {express.Response} res outgoing response, for sending response to client
+ * @param {express.NextFunction} next callback function to pass control to next middleware
+ */
 export function searchTracks(req, res, next){
+	// searchTracks procedure, see /database_docs/procedures/searchTracks.md
 	const query = "CALL searchTracks(?);"
 	const value = req.query.q
 	try{
