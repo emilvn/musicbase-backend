9ef2ca9b4e7a7482ee1e408239977794940ad15e
emilvn <emilvnielsen@hotmail.com>
2023-09-19 11:17:03 +0200

    initial changes

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,8 +1,34 @@
 import { connection } from "../../config/database.js";
 import {validateSearchQuery} from "../validation/search.validation.js";
 
-export function getAllSearchReq(req, res, next){
-	const query = "call search(?);"
+export function searchArtists(req, res, next){
+	const query = "CALL searchArtists(?);"
+	const value = req.query.q
+	try{
+  		validateSearchQuery(value);
+  		connection.query(query, [value], (error, results, _fields) => {
+			if(error) next(error);
+			else res.json(results[0]);
+	  	});
+	}catch(err){
+		next(err);
+	}
+}
+export function searchAlbums(req, res, next){
+	const query = "CALL searchAlbums(?);"
+	const value = req.query.q
+	try{
+  		validateSearchQuery(value);
+  		connection.query(query, [value], (error, results, _fields) => {
+			if(error) next(error);
+			else res.json(results[0]);
+	  	});
+	}catch(err){
+		next(err);
+	}
+}
+export function searchTracks(req, res, next){
+	const query = "CALL searchTracks(?);"
 	const value = req.query.q
 	try{
   		validateSearchQuery(value);
--- src/routers/search.router.js
+++ src/routers/search.router.js
@@ -1,7 +1,9 @@
-import { getAllSearchReq } from "../controllers/search.controller.js";
+import {searchArtists} from "../controllers/search.controller.js";
 
 import express from "express";
 
 export const searchRouter = express.Router();
 
-searchRouter.get("/", getAllSearchReq);
+searchRouter.get("/artists", searchArtists);
+searchRouter.get("/albums", searchArtists);
+searchRouter.get("/tracks", searchArtists);
