fbbc2911c8bb27f74e24cf540e38fd22d89319e4
Bindholt <Cbindholt@gmail.com>
2023-09-20 12:53:56 +0200

    moved dotenv.config

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 import fs from "fs";
 
+dotenv.config();
+
 const dbconfig = {
 	host: process.env.MYSQL_HOST,
 	port: process.env.MYSQL_PORT,
@@ -13,5 +15,4 @@ if (process.env.MYSQL_CERT) {
 	dbconfig.ssl = {cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem")};
 };
 
-dotenv.config();
 export const connection = mysql.createConnection(dbconfig);
