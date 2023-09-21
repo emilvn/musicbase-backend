704ddd292b735c206c4f1054ea8591916e7b0dd9
emilvn <emilvnielsen@hotmail.com>
2023-09-21 11:06:33 +0200

    changed port to 4000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { searchRouter } from "./src/routers/search.router.js";
 import {albumsRouter} from "./src/routers/albums.router.js";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
 app.use(cors());
